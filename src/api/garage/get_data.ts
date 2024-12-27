d84d35c3ed2c7de58e3e4beed0c33962cc0c96a8
Ihar-Batura <A17331582@gmail.com>
2024-12-27 15:49:35 +0300

    fix: fix function getGarageData

--- src/api/garage/get_data.ts
+++ src/api/garage/get_data.ts
@@ -1,8 +1,9 @@
-async function getGarageData(url: string = '/garage') {
+async function getGarageData(url: string) {
   const path = `http://127.0.0.1:3000${url}`;
   const result = await fetch(path);
   const data = await result.json();
-  console.log(data);
+
+  return data;
 }
 
 export default getGarageData;
