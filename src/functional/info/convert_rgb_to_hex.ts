bc789681e6a33e9ea679daa2be9c3d867c5dbaa2
Ihar-Batura <A17331582@gmail.com>
2024-12-28 13:57:42 +0300

    feat: add function convertRgbToHex

---
+++ src/functional/info/convert_rgb_to_hex.ts
@@ -0,0 +1,12 @@
+function convertRgbToHex(color: string) {
+  const arrColor = color.replace(/[rgb()]/g, '').split(',');
+  let result: string = '#';
+  for (let i = 0; i < arrColor.length; i += 1) {
+    let hex: string = (+arrColor[i]).toString(16);
+    const hexColor: string = hex.length === 1 ? '0' + hex : hex;
+    result += hexColor;
+  }
+  return result;
+}
+
+export default convertRgbToHex;
