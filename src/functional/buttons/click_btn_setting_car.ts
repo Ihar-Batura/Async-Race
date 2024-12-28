53db4881745f647595d55e161ed7db5d1b5d4fa1
Ihar-Batura <A17331582@gmail.com>
2024-12-28 13:59:08 +0300

    feat: add function clickBtnSettingCar

---
+++ src/functional/buttons/click_btn_setting_car.ts
@@ -0,0 +1,28 @@
+import changeDisabledUpdateCarElements from '../element/change_disabled_update_car_elements';
+import saveCarIdInUpdateBtn from './save_car_id_in_update_btn';
+import fillUpdateCarInputsInfo from '../info/fill_update_car_inputs_info';
+
+function clickBtnSettingCar(): void {
+  const settingBtnsList: NodeListOf<Element> = document.querySelectorAll(
+    '.car-item__btn-change'
+  );
+
+  if (settingBtnsList.length > 0) {
+    settingBtnsList.forEach((btn) => {
+      btn.addEventListener('click', function (event: Event) {
+        if (event) {
+          if (event.target) {
+            const parent = (event.target as HTMLButtonElement).offsetParent;
+            const parentId: string = (parent as HTMLDivElement).id;
+            console.log(parentId);
+            changeDisabledUpdateCarElements(false);
+            saveCarIdInUpdateBtn(parentId);
+            fillUpdateCarInputsInfo(parentId);
+          }
+        }
+      });
+    });
+  }
+}
+
+export default clickBtnSettingCar;
