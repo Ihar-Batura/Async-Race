13a86b84ec40997008b7976ef4de183bb0d50201
Ihar-Batura <A17331582@gmail.com>
2024-12-28 12:41:15 +0300

    feat: add function changeDisabledElementById

---
+++ src/functional/element/change_disabled_element_by_id.ts
@@ -0,0 +1,12 @@
+function changeDisabledElementById(id: string, action: boolean): void {
+  const element: HTMLElement | null = document.getElementById(`${id}`);
+  if (element) {
+    if (action === true) {
+      element.setAttribute('disabled', 'true');
+    } else {
+      element.removeAttribute('disabled');
+    }
+  }
+}
+
+export default changeDisabledElementById;
