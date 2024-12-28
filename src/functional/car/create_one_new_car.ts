35fbbbddf663fc42dfd67331602eec0b1a95ccc4
Ihar-Batura <A17331582@gmail.com>
2024-12-28 10:54:02 +0300

    fix: fix function createNewCar and add functional

--- src/functional/car/create_one_new_car.ts
+++ src/functional/car/create_one_new_car.ts
@@ -1,11 +1,24 @@
 import getInputValue from '../input/get_input_value';
 import addCarToDB from '../../api/garage/add_car_to_db';
+import whatPageNumber from '../info/what_page_number';
+import getGarageData from '../../api/garage/get_data';
+import addCarsToGaragePageFromDB from './add_cars_to_garage_page_from_db';
+import showSumCarsInGarage from '../info/show_sum_cars_in_garage';
+import cleanInputValue from '../input/clean_input_value';
 
-function createNewCar(id: string) {
+async function createNewCar(id: string) {
   const carName = getInputValue(id + 'name');
   const carColor = getInputValue(id + 'color');
   if (carName.length > 0) {
     addCarToDB(carName, carColor);
+    const pageNumber = whatPageNumber();
+    const garageThisPageData = await getGarageData(
+      `/garage?_page=${pageNumber}&_limit=7`
+    );
+    addCarsToGaragePageFromDB(garageThisPageData);
+    const garageData = await getGarageData('/garage');
+    showSumCarsInGarage(garageData.length);
+    cleanInputValue(id + 'name');
   }
 }
 
