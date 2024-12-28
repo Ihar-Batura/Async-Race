9b01460ab0fb01af663d6a52a7dd2dd51449edd1
Ihar-Batura <A17331582@gmail.com>
2024-12-28 13:59:44 +0300

    feat: add function addListenersBtnsOnGaragePage

---
+++ src/functional/buttons/add_listeners_btns_on_ garage_page.ts
@@ -0,0 +1,7 @@
+import clickBtnSettingCar from './click_btn_setting_car';
+
+function addListenersBtnsOnGaragePage(): void {
+  clickBtnSettingCar();
+}
+
+export default addListenersBtnsOnGaragePage;
