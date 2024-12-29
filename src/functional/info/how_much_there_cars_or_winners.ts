cda806375904863b791fa9a44297df6c69e21651
Ihar-Batura <A17331582@gmail.com>
2024-12-29 15:28:15 +0300

    feat: add function howMatchThereCarsOrWinners

---
+++ src/functional/info/how_much_there_cars_or_winners.ts
@@ -0,0 +1,11 @@
+function howMatchThereCarsOrWinners(className: string): string {
+  const element: HTMLElement | null = document.querySelector(`.${className}`);
+  if (element) {
+    const elementValue: string = element.innerText;
+    const result = elementValue.replace(/\D/g, '');
+    return result;
+  }
+  return '0';
+}
+
+export default howMatchThereCarsOrWinners;
