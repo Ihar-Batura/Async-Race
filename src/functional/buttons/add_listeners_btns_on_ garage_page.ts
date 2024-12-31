a269a70e16c2363039cec75148604a2238e1b0ef
Ihar-Batura <A17331582@gmail.com>
2024-12-31 13:32:15 +0300

    feat: add function clickBtnReturnCar to addListenersBtnsOnGaragePage

--- src/functional/buttons/add_listeners_btns_on_ garage_page.ts
+++ src/functional/buttons/add_listeners_btns_on_ garage_page.ts
@@ -1,9 +1,11 @@
 import clickBtnStartOneCar from './click_btn_start_one_car';
+import clickBtnReturnCar from './click_btn_return_car';
 import clickBtnSettingCar from './click_btn_setting_car';
 import clickBtnDeleteCar from './click_btn_delete_car';
 
 function addListenersBtnsOnGaragePage(): void {
   clickBtnStartOneCar();
+  clickBtnReturnCar();
   clickBtnSettingCar();
   clickBtnDeleteCar();
 }
