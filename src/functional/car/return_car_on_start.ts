c97131ca0447a0f9643c6910b01a68c89fb38994
Ihar-Batura <A17331582@gmail.com>
2024-12-31 13:29:57 +0300

    feat: add function returnCarOnStart

---
+++ src/functional/car/return_car_on_start.ts
@@ -0,0 +1,11 @@
+function returnCarOnStart(id: string) {
+  const parentContainer: HTMLElement | null = document.getElementById(`${id}`);
+  if (parentContainer) {
+    const car: HTMLDivElement | null = parentContainer.querySelector('.car');
+    if (car) {
+      car.style.left = '0px';
+    }
+  }
+}
+
+export default returnCarOnStart;
