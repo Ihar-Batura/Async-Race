0c36a2e7f5b5cd65227adaee988d2e0639591955
Ihar-Batura <A17331582@gmail.com>
2025-01-02 12:22:20 +0300

    feat: add onClick to button Race

--- src/components/garage_control_btns.ts
+++ src/components/garage_control_btns.ts
@@ -1,6 +1,7 @@
 import createElement from '../creating/create_element';
 import createButton from '../creating/create_btn';
 import createOneHundredRandomCars from '../functional/car/create_one_hundred_cars';
+import clickBtnRace from '../functional/buttons/click_btn_race';
 
 function createControlBtns(): HTMLElement {
   const btnsContainer = createElement({
@@ -9,12 +10,16 @@ function createControlBtns(): HTMLElement {
   });
   createButton({
     classes: ['btn', 'cars-control__btn', 'btn-color__green'],
+    id: 'btn-race',
     text: 'Race',
+    onClick: () => clickBtnRace(),
     parent: btnsContainer,
   });
   createButton({
     classes: ['btn', 'cars-control__btn', 'btn-color__red'],
+    id: 'btn-reset',
     text: 'Reset',
+    disabled: true,
     parent: btnsContainer,
   });
   createButton({
