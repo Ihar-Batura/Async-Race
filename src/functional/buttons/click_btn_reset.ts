f45c9ddc72777dbd41e4b96cde9c35f9f1c7e2a2
Ihar-Batura <A17331582@gmail.com>
2025-01-02 14:20:02 +0300

    feat: add deleteFasterCar when click button reset

--- src/functional/buttons/click_btn_reset.ts
+++ src/functional/buttons/click_btn_reset.ts
@@ -1,10 +1,12 @@
 import changeDisabledElementById from '../element/change_disabled_element_by_id';
 import changeDisabledChildElement from '../element/change_disabled_child_element';
 import returnCarOnStart from '../car/return_car_on_start';
+import deleteFasterCar from '../info/delete_faster_car';
 
 function clickBtnReset() {
   changeDisabledElementById('btn-reset', true);
   changeDisabledElementById('btn-race', false);
+  deleteFasterCar();
 
   const carsList: NodeListOf<Element> =
     document.querySelectorAll('.car-container');
