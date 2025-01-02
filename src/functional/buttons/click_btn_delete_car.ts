aa9a2baac3c98f72e5f2447b4c3a4dc243994930
Ihar-Batura <A17331582@gmail.com>
2025-01-02 19:02:09 +0300

    feat: add deleteWinnerFromDB to clickBtnDeleteCar

--- src/functional/buttons/click_btn_delete_car.ts
+++ src/functional/buttons/click_btn_delete_car.ts
@@ -1,4 +1,5 @@
 import deleteCarFromDB from '../../api/garage/delete_car';
+import deleteWinnerFromDB from '../../api/winners/delete_winner';
 
 function clickBtnDeleteCar(): void {
   const deleteBtnsList: NodeListOf<Element> = document.querySelectorAll(
@@ -13,6 +14,7 @@ function clickBtnDeleteCar(): void {
             const parent = (event.target as HTMLButtonElement).offsetParent;
             const parentId: string = (parent as HTMLDivElement).id;
             deleteCarFromDB(parentId);
+            deleteWinnerFromDB(parentId);
           }
         }
       });
