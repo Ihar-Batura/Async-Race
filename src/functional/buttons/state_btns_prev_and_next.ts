853e11ab2d6cc323914a461301a6326a1149a41b
Ihar-Batura <A17331582@gmail.com>
2025-01-03 11:39:06 +0300

    feat: add new functional for winner page to stateBtnsPrevAndNext

--- src/functional/buttons/state_btns_prev_and_next.ts
+++ src/functional/buttons/state_btns_prev_and_next.ts
@@ -1,11 +1,29 @@
 import whatPageNumber from '../info/what_page_number';
 import changeDisabledElementById from '../element/change_disabled_element_by_id';
 import howMatchThereCarsOrWinners from '../info/how_much_there_cars_or_winners';
+import isPageWinners from '../info/is_page_winners';
 
 function stateBtnsPrevAndNext() {
   const page: string = whatPageNumber();
-  const pages: string = howMatchThereCarsOrWinners('garage__cars-amount');
-  const pagesAmount: number = Math.ceil(+pages / 7);
+  const pageWinners: boolean = isPageWinners();
+  let pages;
+  let pagesAmount;
+
+  if (pageWinners) {
+    const pagesWinners: string = howMatchThereCarsOrWinners(
+      'winners__cars-amount'
+    );
+    const pagesWinnersAmount: number = Math.ceil(+pagesWinners / 10);
+    pages = pageWinners;
+    pagesAmount = pagesWinnersAmount;
+  } else {
+    const pagesGarage: string = howMatchThereCarsOrWinners(
+      'garage__cars-amount'
+    );
+    const pagesGarageAmount: number = Math.ceil(+pagesGarage / 7);
+    pages = pagesGarage;
+    pagesAmount = pagesGarageAmount;
+  }
 
   if (page === '1') {
     changeDisabledElementById('btn-prev', true);
