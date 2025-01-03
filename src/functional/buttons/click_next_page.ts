b33fcaffec1c74bd7a9e052de7dec8247c06da49
Ihar-Batura <A17331582@gmail.com>
2025-01-03 16:33:55 +0300

    feat: add check what page

--- src/functional/buttons/click_next_page.ts
+++ src/functional/buttons/click_next_page.ts
@@ -5,18 +5,28 @@ import addListenersBtnsOnGaragePage from './add_listeners_btns_on_ garage_page';
 import stateBtnsPrevAndNext from './state_btns_prev_and_next';
 import changePageNumber from '../info/change_page_number';
 import changeDisabledElementById from '../element/change_disabled_element_by_id';
+import isPageWinners from '../info/is_page_winners';
+import updateWinnersPage from '../page/update_winners_page';
 
 async function nextPage() {
-  const pageNumber = whatPageNumber();
-  const nextPageData = await getGarageData(
-    `/garage?_page=${+pageNumber + 1}&_limit=7`
-  );
-  addCarsToGaragePageFromDB(nextPageData);
-  addListenersBtnsOnGaragePage();
-  changePageNumber(pageNumber, '+');
-  stateBtnsPrevAndNext();
-  changeDisabledElementById('btn-reset', true);
-  changeDisabledElementById('btn-race', false);
+  const pageWinners: boolean = isPageWinners();
+  if (pageWinners) {
+    const pageNumber = whatPageNumber();
+    changePageNumber(pageNumber, '+');
+    updateWinnersPage('id');
+    stateBtnsPrevAndNext();
+  } else {
+    const pageNumber = whatPageNumber();
+    const nextPageData = await getGarageData(
+      `/garage?_page=${+pageNumber + 1}&_limit=7`
+    );
+    addCarsToGaragePageFromDB(nextPageData);
+    addListenersBtnsOnGaragePage();
+    changePageNumber(pageNumber, '+');
+    stateBtnsPrevAndNext();
+    changeDisabledElementById('btn-reset', true);
+    changeDisabledElementById('btn-race', false);
+  }
 }
 
 export default nextPage;
