9356f549c3d7f3eb31a14908b332925b886df9bd
Ihar-Batura <A17331582@gmail.com>
2025-01-04 10:29:31 +0300

    fix: fix function sortTime add delete classes from sort buttons

--- src/functional/winners/click_sort_time.ts
+++ src/functional/winners/click_sort_time.ts
@@ -10,13 +10,21 @@ export default function clickSortOnTime() {
 
 function sortWins() {
   const btnSortTime = document.querySelector('.table-record');
-  if (btnSortTime) {
-    if (!btnSortTime.classList.value.includes('sort')) {
+  const btnSortWins = document.querySelector('.table-wins');
+
+  if (btnSortTime && btnSortWins) {
+    if (!btnSortTime.classList.value.includes('sort-time__asd')) {
       updateWinnersPage(`time&_order=ASC`);
-      btnSortTime.classList.add('sort');
+      btnSortTime.classList.add('sort-time__asd');
+      btnSortTime.classList.remove('sort-time__desc');
+      btnSortWins.classList.remove('sort-wins__asd');
+      btnSortWins.classList.remove('sort-wins__desc');
     } else {
       updateWinnersPage(`time&_order=DESC`);
-      btnSortTime.classList.remove('sort');
+      btnSortTime.classList.add('sort-time__desc');
+      btnSortTime.classList.remove('sort-time__asd');
+      btnSortWins.classList.remove('sort-wins__asd');
+      btnSortWins.classList.remove('sort-wins__desc');
     }
   }
 }
