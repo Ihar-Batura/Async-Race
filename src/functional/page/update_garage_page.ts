318d84799a87e9e59b5a2033b3794846041d4ca1
Ihar-Batura <A17331582@gmail.com>
2025-01-05 10:54:06 +0300

    fix: fix update_garage_page.ts add function stateBtnsPrevAndNext;

--- src/functional/page/update_garage_page.ts
+++ src/functional/page/update_garage_page.ts
@@ -3,16 +3,19 @@ import getGarageData from '../../api/garage/get_data';
 import addCarsToGaragePageFromDB from '../car/add_cars_to_garage_page_from_db';
 import showSumCarsInGarage from '../info/show_sum_cars_in_garage';
 import addListenersBtnsOnGaragePage from '../buttons/add_listeners_btns_on_ garage_page';
+import stateBtnsPrevAndNext from '../buttons/state_btns_prev_and_next';
 
 async function updateGaragePage() {
   const pageNumber = whatPageNumber();
   const garageThisPageData = await getGarageData(
     `/garage?_page=${pageNumber}&_limit=7`
   );
-  addCarsToGaragePageFromDB(garageThisPageData);
+
   const garageData = await getGarageData('/garage');
   const carsInGarage: number = garageData.length > 0 ? garageData.length : 0;
+  addCarsToGaragePageFromDB(garageThisPageData);
   showSumCarsInGarage(carsInGarage);
+  stateBtnsPrevAndNext();
   addListenersBtnsOnGaragePage();
 }
 
